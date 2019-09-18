aa864f40fde90445790f461983ca23923fc9333a
Jorge Vasquez <ing.jorgevasquezpro@gmail.com>
2019-09-18 12:06:02 -0500

    cambio de comillas dobles por simples

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -5,8 +5,8 @@ const productsRouter = require('./routes/products')
 
 app.use('/products', productsRouter);
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 const server = app.listen(8000, function(){
     console.log(`listening in port http://localhost:${server.address().port}`);
